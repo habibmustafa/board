4f95792939cc01cb3603f3305817950c521668a5
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2024-07-09 18:27:22 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+
   return (
     <>
       <Routes>
-        <Route path="/:id" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,13 +2,11 @@ import { onDisconnect, onValue, ref, set } from "firebase/database";
 import { useEffect, useState } from "react";
 import { db } from "src/firebase";
 import { useMouse } from "@uidotdev/usehooks";
-import { useParams } from "react-router-dom";
 
 function Dashboard() {
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<any>(null);
   const [mouse] = useMouse();
-
-  const { id } = useParams();
+  const [id, setId] = useState<any>(null);
 
   useEffect(() => {
     if (!id) return;
@@ -18,67 +16,57 @@ function Dashboard() {
     });
   }, [mouse, id]);
 
-  // const getConnections = async () => {
-  //   get(ref(db, "/connection/")).then((snapshot) => {
-  //     if (snapshot.size === 2) {
-  //       setUser(0);
-  //     } else {
-  //       setUser(!!snapshot.hasChild("1") ? 2 : 1);
-  //     }
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   getConnections();
-  // }, []);
-
-  // useEffect(() => {
-  //   if (!ip) return;
-
-  //   // Example code to get the client IP address using WebRTC
-  //   const connection = ref(db, "/connection/" + ip);
-
-  //   set(connection, {
-  //     position: {
-  //       x: 300,
-  //       y: 300,
-  //     },
-  //     value: "",
-  //   });
-
-  //   onValue(connection, (snapshot) => {
-  //     setData((prev: any) => ({
-  //       ...prev,
-  //       ...snapshot.val(),
-  //     }));
-  //   });
-  //   onDisconnect(connection).remove();
-  // }, [ip]);
-
   useEffect(() => {
     onValue(ref(db, "/connection/"), (snapshot) => {
       if (snapshot.exists()) {
-        setData(snapshot.val());
-        console.log(snapshot.val());
+        setData(Object.values(snapshot.exportVal()));
+      } else {
+        setData([]);
       }
     });
-
-    onDisconnect(ref(db, "/connection/" + id)).remove();
   }, []);
+
+  useEffect(() => {
+    onDisconnect(ref(db, "/connection/" + id)).remove();
+  }, [id]);
+
+  const handleConnectionSelect = (userId: number) => {
+    setId(userId);
+    set(ref(db, `/connection/${userId}`), { id: userId });
+  };
 
   return (
     <div className="h-screen overflow-hidden relative">
-      {data.map((item: any, index: number) => (
-        <input
-          key={index}
-          className="px-2 py-1 bg-slate-700 rounded text-white absolute -translate-x-1/2 -translate-y-1/2 cursor-none"
-          style={{ left: item.position.x, top: item.position.y }}
-          value={item.value}
-          onChange={(e) => {
-            set(ref(db, "/connection/" + id + "/value"), e.target.value);
-          }}
-        />
-      ))}
+      {!!id &&
+        data?.map((item: any, index: number) => (
+          <input
+            key={index}
+            autoFocus
+            className="px-2 py-1 bg-slate-700 rounded text-white absolute -translate-x-1/2 -translate-y-1/2 cursor-none"
+            style={{ left: item.position?.x, top: item.position?.y }}
+            value={item?.value}
+            onChange={(e) => {
+              set(ref(db, "/connection/" + id + "/value"), e.target.value);
+            }}
+          />
+        ))}
+
+      {!id && data !== null && (
+        <div className="flex justify-center items-center h-full gap-8">
+          {[1, 2].map(
+            (id) =>
+              data.every((item: any) => item.id !== id) && (
+                <button
+                  key={id}
+                  className="w-36 h-32 text-4xl bg-slate-200 rounded-md hover:bg-slate-300 transition-all"
+                  onClick={() => handleConnectionSelect(id)}
+                >
+                  {id}
+                </button>
+              )
+          )}
+        </div>
+      )}
     </div>
   );
 }
