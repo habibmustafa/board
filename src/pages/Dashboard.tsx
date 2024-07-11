85a662da98e868709f2e4345601bf2b47b979a98
Habibullah Mustafazadeh <hmustafazadeh@its.gov.az>
2024-07-11 12:40:23 +0400

    update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -24,16 +24,29 @@ const Dashboard: React.FC = () => {
 
       <div className="h-screen overflow-hidden relative bg-neutral-100">
         {deviceId &&
-          data?.map((user: any, index: number) => (
-            <TextArea
-              key={index}
-              label={`${user.id} ${user.id === deviceId ? "- You" : ""}`}
-              value={user?.value || ""}
-              position={user.position || { x: 0, y: 0 }}
-              onChange={(value) => updateValue(value)}
-              autoFocus={user.id === deviceId}
-            />
-          ))}
+          data?.map((user: any, index: number) =>
+            user.id ? (
+              <TextArea
+                key={index}
+                label={`${user.id} ${user.id === deviceId ? "- You" : ""}`}
+                value={user?.value || ""}
+                position={user.position || { x: 0, y: 0 }}
+                onChange={(value) => updateValue(value)}
+                autoFocus={user.id === deviceId}
+              />
+            ) : (
+              <div className="flex h-full w-full justify-center items-center bg-gray-400 rounded-md">
+                <button
+                  className="w-36 py-3 rounded-md text-lg font-medium bg-slate-900 text-white transition-colors hover:bg-slate-800"
+                  onClick={() => {
+                    window.location.reload();
+                  }}
+                >
+                  Yenilə
+                </button>
+              </div>
+            )
+          )}
       </div>
     </>
   );
